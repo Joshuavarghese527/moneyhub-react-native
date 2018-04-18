7203c06f28071895bf1dfc233b34f6f11e80a77b
Joshuavarghese527 <joshuavarghese527@gmail.com>
2018-04-17 20:13:52 -0400

    added text and folder structure

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import { sanFranciscoWeights } from 'react-native-typography'
 import { StyleSheet, Text, View } from 'react-native';
+import { text } from "./src/style/text";
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Text style={[sanFranciscoWeights.black, text.title1]}>Open up App.js to start working on your app!</Text>
+        <Text style={sanFranciscoWeights.medium}>Changes you make will automatically reload.</Text>
+        <Text style={sanFranciscoWeights.regular}>Shake your phone to open the developer menu.</Text>
       </View>
     );
   }
